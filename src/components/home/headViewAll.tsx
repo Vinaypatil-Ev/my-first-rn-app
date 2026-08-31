2c88ea53173a61174289ff7c29b3419dfb5ca220
vinaypatil <vinaypatil153@gamil.com>
2026-08-31 16:00:21 +0530

    feat: update HeadViewAll component to use 'title' prop instead of 'heading' and adjust text size

--- src/components/home/headViewAll.tsx
+++ src/components/home/headViewAll.tsx
@@ -1,9 +1,9 @@
 import { Text, View } from "react-native";
 
-const HeadViewAll = ({ heading }: { heading: string }) => {
+const HeadViewAll = ({ title }: { title: string }) => {
   return (
     <View className="flex flex-row mt-6 justify-between">
-      <Text className="text-4xl font-sans-bold text-primary">{heading}</Text>
+      <Text className="text-3xl font-sans-bold text-primary">{title}</Text>
       <Text className="px-4 py-1 text-primary text-2xl font-sans-medium border-2 rounded-4xl border-black/20">
         View All
       </Text>
